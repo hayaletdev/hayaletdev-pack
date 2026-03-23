643823b7a2fc70187b998e898e7c61c9719fa6d0
hayaletdev <sarisulukd@gmail.com>
2026-03-23 04:16:32 +0300

    battlepass ui config: add labels for extra craft npc args

--- root/battlepassconfig.py
+++ root/battlepassconfig.py
@@ -2,6 +2,8 @@ CRAFT_ARGS = {
 	0: "Herhangi Bir Uretim Yap",
 	1: "Aura Uret",
 	20018: "Baek-Go Uretim Yap",
+	20017: "NPC 20017 Uretim Yap",
+	20022: "NPC 20022 Uretim Yap",
 }
 
 TASK_TYPE_NAMES = {
